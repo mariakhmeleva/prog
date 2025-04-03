44ea0fafa74f459c606f5add39fea1e50bb604b8
Мария Хмелева <ria2201@mail.ru>
2025-04-03 19:06:04 +0700

    Раздел 1 задание 1.2

--- lab2_1_1.py
+++ lab2_1_1.py
@@ -1,10 +1,29 @@
 #1/1
-x, y, z = input('Введите число '), input('Введите число '), input('Введите число    ')
-try:
-    x = int(x)
-    y = int(y)
-    z = int(z)
-    print('Минимальное число ',min(x,y,z))
-except:
-    print('Вы ввели не число')
+# x = input('Введите число: ')
+# y = input('Введите число: ')
+# z = input('Введите число: ')
 
+# try:
+#     x = int(x)
+#     y = int(y)
+#     z = int(z)
+#     print('Минимальное число:', min(x, y, z))
+# except ValueError:
+#     print('Вы ввели не число')
+#1/2
+numbers = []
+for _ in range(3):
+    user_input = input('Введите число: ')
+    try:
+        number = int(user_input)
+    except ValueError:
+        print('Вы ввели не число')
+        number = None 
+    numbers.append(number)
+
+filtered_numbers = []
+for num in numbers:
+    if num is not None and 1 <= num < 50:
+        filtered_numbers.append(num)
+
+print('Ваши числа входящие в промежуток 1-50:', filtered_numbers)
