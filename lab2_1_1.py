64090fbd98f8a9444e4e43efd1ddb0a2356488ed
Мария Хмелева <ria2201@mail.ru>
2025-04-03 19:08:58 +0700

    Раздел 1 задание 1.3

--- lab2_1_1.py
+++ lab2_1_1.py
@@ -11,19 +11,27 @@
 # except ValueError:
 #     print('Вы ввели не число')
 #1/2
-numbers = []
-for _ in range(3):
-    user_input = input('Введите число: ')
-    try:
-        number = int(user_input)
-    except ValueError:
-        print('Вы ввели не число')
-        number = None 
-    numbers.append(number)
+# numbers = []
+# for _ in range(3):
+#     user_input = input('Введите число: ')
+#     try:
+#         number = int(user_input)
+#     except ValueError:
+#         print('Вы ввели не число')
+#         number = None 
+#     numbers.append(number)
 
-filtered_numbers = []
-for num in numbers:
-    if num is not None and 1 <= num < 50:
-        filtered_numbers.append(num)
+# filtered_numbers = []
+# for num in numbers:
+#     if num is not None and 1 <= num < 50:
+#         filtered_numbers.append(num)
 
-print('Ваши числа входящие в промежуток 1-50:', filtered_numbers)
+# print('Ваши числа входящие в промежуток 1-50:', filtered_numbers)
+
+#1/3
+try:
+    number = float(input('Введите число: '))
+    for i in range(1, 11):
+        print(f'{number} * {i} = {number * i}')
+except ValueError:
+    print('Ошибка: вы ввели не число')
